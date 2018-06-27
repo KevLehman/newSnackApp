abadb8afde236140b99353f264ef7eb60dd00b06
Kevin <kaleman960@gmail.comcls>
2018-06-27 12:56:03 -0500

    Now likes are counted from table on each like request

--- api/controllers/likes.js
+++ api/controllers/likes.js
@@ -3,29 +3,34 @@ const { Likes, Products } = require('../models')
 const likeProduct = (req, res, next) => {
     const { id } = req.user
     const { productId } = req.params
+    let likesCount = 0
 
     Likes.findOne({
         where: {productId: productId, userId: id}
     }).then(like =>{
         if(like) return res.status(200).send({status: "You liked this product"})
+        let newLike = Likes.build({
+            productId: productId,
+            userId: id
+        })
+        return newLike.save()
+    }).then(newLike => {
+        return Likes.findAndCountAll({
+            where: {productId: productId}
+        })
+    }).then(likes => {
+        likesCount = likes.count
         return Products.findOne({
             where: {id: productId}
         })
     }).then(snack => {
         if(!snack) return res.status(404).send([])
-        snack.likes += 1
+        snack.likes = likesCount
         return snack.save()
-    }).then(snack => {
-        const newLike = Likes.build({
-            userId: id,
-            productId: productId
-        })
-        return newLike.save()
-    }).then((like) => {
-        if(like) return res.status(200).send({status: "You liked this product"})
-    })
-    .catch(err => {
-        res.status(500).send(err)
+    }).then(newSnack => {
+        return res.status(200).send({status: "You liked this product"})
+    }).catch(err => {
+        return res.status(500).send(err)
     })
 }
 
