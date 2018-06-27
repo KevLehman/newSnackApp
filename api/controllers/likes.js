0cfee38ec2e57dd9cd4d06578d5a1414bbef55a5
Kevin <kaleman960@gmail.com>
2018-06-27 13:16:01 -0500

    Updated dislike function to count likes on every deletion

--- api/controllers/likes.js
+++ api/controllers/likes.js
@@ -37,26 +37,29 @@ const likeProduct = (req, res, next) => {
 const dislikeProduct = (req, res, next) => {
     const { id } = req.user
     const { productId } = req.params
+    let likesCount = 0
 
     Likes.findOne({
         where: {productId: productId, userId: id}
     }).then(like =>{
         if(!like) return res.status(200).send({status: "You disliked this product"})
-        
         return Likes.destroy({
             where: {productId: productId, userId: id}
         })
     }).then(() => {
+        return Likes.findAndCountAll({
+            where: {productId: productId}
+        })
+    }).then(likes => {
+        likesCount = likes.count
         return Products.findOne({
             where: {id: productId}
         })
     }).then(snack => {
         if(!snack) return res.status(404).send([])
-        snack.likes -= 1
-
+        snack.likes = likesCount
         return snack.save()
     }).then(newsnack => {
-
         if(newsnack) return res.status(200).send({status: "You disliked this product"})
     }).catch(err => {
         res.status(500).send(err)
